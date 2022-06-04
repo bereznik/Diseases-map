d7a2aac8e468001358173063418e7747e375bf0a
alavinsky.inoa <alavinsky@inoa.com.br>
2022-06-04 19:39:28 -0300

    fix: Validação de usuário e senha não encontrados

--- web/diseasesmap/core/views.py
+++ web/diseasesmap/core/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpRequest
-from itsdangerous import json
+import json
 
 from types import SimpleNamespace
 from django.contrib import messages
 from server import views
 
+
 def dashboard(request):
     return render(request, 'dashboard.html', {})
+
 
 def about_restrict(request):
     return render(request, 'about_restrict.html', {})
 
+
 def contact_restrict(request):
     return render(request, 'contact_restrict.html', {})
+
 
 def about(request):
     return render(request, 'about.html', {})
 
+
 def contact(request):
     return render(request, 'contact.html', {})
+
 
 @csrf_exempt
 def login(request):
@@ -30,28 +36,37 @@ def login(request):
 
         myRequest = HttpRequest()
         myRequest.method = 'GET'
-        jsonResponse = views.usuariosApi(myRequest,username)
-        dictResponse = json.loads(jsonResponse.content, object_hook=lambda d: SimpleNamespace(**d))
-        
-        if password == dictResponse.senha:
-            return index(request)
-        else:
-            messages.error(request,"Usuário e senha não encontrados")
-            return render(request, 'login.html', {})
-            
+
+        try:
+            jsonResponse = views.usuariosApi(myRequest, username)
+            dictResponse = json.loads(
+                jsonResponse.content, object_hook=lambda d: SimpleNamespace(**d))
+            if password == dictResponse.senha:
+                return index(request)
+            else:
+                messages.error(request, "Senha não encontrada")
+                return render(request, 'login.html', {})
+        except:
+            messages.error(request, "Usuário não encontrado")
+
     return render(request, 'login.html', {})
+
 
 def index(request):
     return render(request, 'index.html', {})
 
+
 def notifications(request):
     return render(request, 'notifications.html', {})
+
 
 def account(request):
     return render(request, 'account.html', {})
 
+
 def usertable(request):
     return render(request, 'user_table.html', {})
+
 
 def diseases(request):
     return render(request, 'diseases.html', {})
