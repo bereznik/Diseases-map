6b0e5251b27a30017706dcf5a2d677d49134359e
bereznik <bernardreznik@gmail.com>
2022-06-06 00:56:44 -0300

    dataframe de verdade

--- web/diseasesmap/core/views.py
+++ web/diseasesmap/core/views.py
@@ -322,11 +322,17 @@ def populate(request):
     return render(request, 'populate.html', {})
 
 def get_notifications():
+    
+    db_connection = sql.connect(host='127.0.0.1', database='diseasesmapdb', user='diseasesmapadmin',
+                                password='diseasesmap')
+    
     QUERY = ''' SELECT l.longitude AS long, l.latitude AS lat, l.nome as municipio, n.nomedoenca AS nomedoenca, n.casos AS casosTotais
                 FROM Notificacoes n JOIN Localidades l ON n.idmunicipio = l.id '''
 
-    df = pd.DataFrame(data ={'lat':[-22.9068,-22.9068,1,1],'long':[-43.1729,-43.1729,3,3],'municipio':['rio','rio','sp','sp'],
-    'nome':['Dengue','Chicungunha','Dengue','COVID'],'casosTotais':[1,2,3,4]})
+    df = pd.read_sql(QUERY,db_connection)
+    
+    # df = pd.DataFrame(data ={'lat':[-22.9068,-22.9068,1,1],'long':[-43.1729,-43.1729,3,3],'municipio':['rio','rio','sp','sp'],
+    # 'nome':['Dengue','Chicungunha','Dengue','COVID'],'casosTotais':[1,2,3,4]})
 
     cols = ['lat','long','municipio']
     json = df.groupby(cols).apply(lambda g: g.drop(cols, axis=1).to_dict('records')).reset_index().rename({0:'doencas'}, axis=1).to_dict('records')
